475e35986078aa519e284a7ac5779b176ea989ea
tobias <72387477+tobwil@users.noreply.github.com>
2023-05-29 14:09:02 +0200

    Update location.js
    changed text Color to white as not visible anymore after iOS update

--- location/location.js
+++ location/location.js
@@ -1,31 +1,32 @@
-// Erstelle ein neues ListWidget-Objekt
+// Create a new ListWidget object
 let widget = new ListWidget()
 
-// Hintergrundbild hinzufügen
+// Add a background image
 let url = "https://eoimages.gsfc.nasa.gov/images/imagerecords/90000/90008/europe_vir_2016.jpg"
 let req = new Request(url)
 let image = await req.loadImage()
 widget.backgroundImage = image
 
-
-// Hole den aktuellen Standort als Objekt mit Breiten- und Längengrad
+// Get the current location as an object with latitude and longitude
 let location = await Location.current()
 
-// Wandle den Standort in eine Adresse um
+// Convert the location into an address
 let address = await Location.reverseGeocode(location.latitude, location.longitude)
 
-// Füge die Adresse als Text zum Widget hinzu
-let text1 = widget.addText("Mein Standort")
+// Add the address as text to the widget
+let text1 = widget.addText("My Location")
 text1.centerAlignText()
 text1.font = Font.boldSystemFont(12)
-widget.addSpacer(10) // Fügt einen Abstand von 10 Pixeln hinzu
+text1.textColor = Color.white() // Change text color to white
+widget.addSpacer(10) // Adds a space of 10 pixels
 let text2 = widget.addText(address[0].name + ", " + address[0].postalCode + ", " + address[0].locality + ", " + address[0].administrativeArea + ", " + address[0].isoCountryCode)
 text2.centerAlignText()
 text2.font = Font.systemFont(12)
+text2.textColor = Color.white() // Change text color to white
 
-// Setze die URL für das Öffnen von Google Maps mit der Adresse
+// Set the URL for opening Google Maps with the address
 widget.url = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(address[0].name)}+${encodeURIComponent(address[0].postalCode)}+${encodeURIComponent(address[0].locality)}+${encodeURIComponent(address[0].administrativeArea)}+${encodeURIComponent(address[0].isoCountryCode)}`
 
-// Zeige das Widget an
+// Display the widget
 Script.setWidget(widget)
 console.log(address)
